ff97074f1f3a75f6fdcbcff052084569a770356b
bank011 <thaimixdj@gmail.com>
2026-07-10 03:09:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-from state import load_state
+from youtube import get_latest as youtube_latest
+from supercell import get_latest as supercell_latest
+
+from classifier import is_important
+
 from line import send_message
+
+from state import load_state, save_state
+
+
+def process(news):
+
+    if news is None:
+        return
+
+    state = load_state()
+
+    last = state.get(news["source"])
+
+    if last == news["id"]:
+        print("Duplicate")
+        return
+
+    if not is_important(news["title"]):
+        print("Skip")
+        return
+
+    send_message(
+        f"""🎮 {news["source"]}
+
+{news["title"]}
+
+{news["link"]}
+"""
+    )
+
+    state[news["source"]] = news["id"]
+
+    save_state(state)
 
 
 def main():
 
-    state = load_state()
+    process(youtube_latest())
 
-    send_message(
-        "✅ Hay Day News Bot v1 เริ่มทำงานแล้ว"
-    )
-
-    print(state)
+    process(supercell_latest())
 
 
 if __name__ == "__main__":
