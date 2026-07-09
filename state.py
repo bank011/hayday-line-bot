73c0d0caa7ab7655ffdf565c6da8e55c31255b69
bank011 <thaimixdj@gmail.com>
2026-07-10 03:27:46 +0700

    Update state.py

--- state.py
+++ state.py
@@ -1,17 +1,36 @@
 import json
 import os
 
-FILE = "state.json"
+from config import STATE_FILE
 
 
-def load():
-    if not os.path.exists(FILE):
+def load_state():
+    if not os.path.exists(STATE_FILE):
         return {}
 
-    with open(FILE, "r", encoding="utf-8") as f:
+    with open(STATE_FILE, "r", encoding="utf-8") as f:
         return json.load(f)
 
 
-def save(data):
-    with open(FILE, "w", encoding="utf-8") as f:
-        json.dump(data, f, ensure_ascii=False, indent=2)
+def save_state(state):
+    with open(STATE_FILE, "w", encoding="utf-8") as f:
+        json.dump(
+            state,
+            f,
+            ensure_ascii=False,
+            indent=4
+        )
+
+
+def is_duplicate(source, news_id):
+
+    state = load_state()
+
+    if state.get(source) == news_id:
+        return True
+
+    state[source] = news_id
+
+    save_state(state)
+
+    return False
