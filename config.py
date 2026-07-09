c1cdbe052947d784a87e0158b5a38cc5e45f8726
bank011 <thaimixdj@gmail.com>
2026-07-10 03:27:28 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,5 @@ import os
 LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
 LINE_GROUP_ID = os.environ["LINE_GROUP_ID"]
 
-YOUTUBE_RSS = "https://www.youtube.com/feeds/videos.xml?channel_id=UC27cEfYbMEA006oOOKc9hKA"
-
-SUPERCELL_RSS = "https://supercell.com/en/rss/"
-
+# ไฟล์เก็บข่าวล่าสุด
 STATE_FILE = "state.json"
