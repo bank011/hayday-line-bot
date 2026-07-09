62d7a61d5ed52d66166c1f04a4ed62d90ac64ccd
bank011 <thaimixdj@gmail.com>
2026-07-10 03:04:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,17 @@
-import os
-import feedparser
-import requests
+from state import load_state
+from line import send_message
 
-CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
-GROUP_ID = os.environ["LINE_GROUP_ID"]
 
-RSS = "https://supercell.com/en/rss/"
+def main():
 
-feed = feedparser.parse(RSS)
+    state = load_state()
 
-hayday = None
+    send_message(
+        "✅ Hay Day News Bot v1 เริ่มทำงานแล้ว"
+    )
 
-for entry in feed.entries:
-    title = entry.title.lower()
+    print(state)
 
-    if "hay day" in title:
-        hayday = entry
-        break
 
-if hayday is None:
-    print("No Hay Day news.")
-    exit(0)
-
-message = {
-    "to": GROUP_ID,
-    "messages": [
-        {
-            "type": "text",
-            "text": f"""🎮 Hay Day News
-
-{hayday.title}
-
-{hayday.link}
-"""
-        }
-    ]
-}
-
-headers = {
-    "Authorization": f"Bearer {CHANNEL_ACCESS_TOKEN}",
-    "Content-Type": "application/json"
-}
-
-res = requests.post(
-    "https://api.line.me/v2/bot/message/push",
-    headers=headers,
-    json=message
-)
-
-print(res.status_code)
-print(res.text)
+if __name__ == "__main__":
+    main()
