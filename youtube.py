0861720a1801f514cb7411523d49df4f289da8cb
bank011 <thaimixdj@gmail.com>
2026-07-10 03:19:13 +0700

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -1,10 +1,8 @@
 import feedparser
 
-RSS = "https://www.youtube.com/@hayday"
-
+RSS = "https://www.youtube.com/feeds/videos.xml?channel_id=UC27cEfYbMEA006oOOKc9hKA"
 
 def get_latest():
-
     feed = feedparser.parse(RSS)
 
     if not feed.entries:
