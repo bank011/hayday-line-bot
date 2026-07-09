3776ebfb23198f44dfe541a4f41a3cb296333d39
bank011 <thaimixdj@gmail.com>
2026-07-10 04:22:02 +0700

    Update state.py

--- state.py
+++ state.py
@@ -1,12 +1,12 @@
 import json
 import os
 
-from config import STATE_FILE
+STATE_FILE = "state.json"
 
 
 def load_state():
     if not os.path.exists(STATE_FILE):
-        return {}
+        return {"sent": []}
 
     with open(STATE_FILE, "r", encoding="utf-8") as f:
         return json.load(f)
@@ -14,23 +14,21 @@ def load_state():
 
 def save_state(state):
     with open(STATE_FILE, "w", encoding="utf-8") as f:
-        json.dump(
-            state,
-            f,
-            ensure_ascii=False,
-            indent=4
-        )
+        json.dump(state, f, ensure_ascii=False, indent=2)
 
 
-def is_duplicate(source, news_id):
+def is_duplicate(news_id):
+    state = load_state()
+    return news_id in state["sent"]
 
+
+def mark_sent(news_id):
     state = load_state()
 
-    if state.get(source) == news_id:
-        return True
+    if news_id not in state["sent"]:
+        state["sent"].append(news_id)
 
-    state[source] = news_id
+        # เก็บล่าสุดไม่เกิน 100 ข่าว
+        state["sent"] = state["sent"][-100:]
 
-    save_state(state)
-
-    return False
+        save_state(state)
