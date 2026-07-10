0ecc002262461d143bf6a9824ce044d8203d6361
bank011 <thaimixdj@gmail.com>
2026-07-10 22:51:10 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 import os
 
 LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
-LINE_GROUP_ID = os.environ["LINE_GROUP_ID"]
 
-# ไฟล์เก็บข่าวล่าสุด
-STATE_FILE = "state.json"
+LINE_GROUP_ID = os.environ["LINE_GROUP_ID"]
